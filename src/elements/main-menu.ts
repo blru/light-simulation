3cd06d7892119317ca0f87dfa4166d76f2b786cd
Blerru <44450511+blru@users.noreply.github.com>
2025-01-01 10:33:48 -0500

    impr: made main menu slightly more accessible

--- src/elements/main-menu.ts
+++ src/elements/main-menu.ts
@@ -9,15 +9,16 @@ const mainMenu = document.createElement("div");
 mainMenu.classList.add("island");
 
 // Functions for manipulating menu
-export const toggle = () => mainMenu.classList.toggle("open");
 export const open = () => mainMenu.classList.add("open");
 export const close = () => mainMenu.classList.remove("open");
 export const isOpen = () => mainMenu.classList.contains("open");
+export const toggle = () => (isOpen() ? close() : open());
 
 const mainMenuButton = document.createElement("button");
 mainMenuButton.classList.add("island");
 mainMenuButton.innerHTML = `<i class="fa-solid fa-bars"></i>`;
 mainMenuButton.addEventListener("click", toggle);
+mainMenuButton.ariaLabel = "Open Main Menu";
 
 mainMenuContainer.append(mainMenuButton, mainMenu); // append main menu after main menu button
 
