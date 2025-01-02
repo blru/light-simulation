d43a3e2c858e4083cc35c110548b7de4fc6c7c01
Blerru <44450511+blru@users.noreply.github.com>
2025-01-02 14:15:03 -0500

    fix: grouped export in elements index

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -2,6 +2,6 @@ export * as simulationView from "./simulation-view";
 export * from "./app-container";
 export * as overlay from "./overlay";
 export * as contextMenu from "./context-menu";
-export * from "./main-menu";
+export * as mainMenu from "./main-menu";
 export * as toolbar from "./toolbar";
 export * as zoomControls from "./time-controls";
