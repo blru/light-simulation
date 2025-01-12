115f17cb5e6da4505403f32a97fe7d32c6ab5e64
Blerru <44450511+blru@users.noreply.github.com>
2025-01-12 14:33:56 -0500

    fix(vector2): fixed magnitude getter referencing itself instead of magnitudeSquared

--- src/simulation/math/vector-2.ts
+++ src/simulation/math/vector-2.ts
@@ -1,8 +1,20 @@
 export class Vector2 {
     constructor(
-        public x: number,
-        public y: number,
+        public x: number = 0,
+        public y: number = 0,
     ) {}
+
+    /**
+     * A readonly zero vector
+     */
+    static zero = Object.freeze(new Vector2(0, 0));
+
+    /**
+     * Returns a new vector that is a unit vector of this vector
+     */
+    normalize() {
+        return new Vector2(this.x / this.magnitude, this.y / this.magnitude);
+    }
 
     /**
      * Returns a new vector that is the sum of this vector and another
@@ -19,7 +31,7 @@ export class Vector2 {
     }
 
     /**
-     * Returns a new vector that is the scalar product of this vector and a scalar value
+     * Returns a new vector whose components are multiplied by the provided scalar value
      */
     multiplyScalar(rhs: number) {
         return new Vector2(this.x * rhs, this.y * rhs);
@@ -46,11 +58,18 @@ export class Vector2 {
         return this.sub(other).magnitude;
     }
 
+    /**
+     * Returns true if the other vector is equal to this vector
+     */
+    equals(other: Vector2) {
+        return this.x === other.x && this.y === other.y;
+    }
+
     get magnitudeSquared() {
         return this.x ** 2 + this.y ** 2;
     }
 
     get magnitude(): number {
-        return Math.sqrt(this.magnitude);
+        return Math.sqrt(this.magnitudeSquared);
     }
 }
